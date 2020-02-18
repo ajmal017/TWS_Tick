0da8ab101da510553dbd0daaf3b1a5633fde05f1
Tobias Seidel <tobiasseidel@mail.de>
2020-02-18 09:58:29 +0100

    Cleaned tws_main.py minor gui improvements

--- tws_main.py
+++ tws_main.py
@@ -10,26 +10,30 @@ import time
 
 class Application(Frame):
 
-
 	def __init__(self, master):
-			ttk.Frame.__init__(self,master)
+		ttk.Frame.__init__(self,master)
 
-			#create GUI
-			self.grid()
-			self.create_widgets()
-			self.account_code = None
-			self.symbol_id, symbol = 0, 'EUR.USD'
+		#create GUI
+		self.grid()
+		self.create_widgets()
+		self.account_code = None
+		self.symbol_id, symbol = 0, 'EUR.USD'
 
 	def create_widgets(self):
-		myfont = ('Lucida Grande',10)
+		myfont = ('Helvetica',10)
 
 		#csv button
 		self.btnconnect = ttk.Button(self, text='CSV', command=self.tick_csv)
-		self.btnconnect.grid(row=0, column=0)
+		self.btnconnect.grid(row=0, column=0, sticky=E)
 
 		#disconnect button
-		self.btndisconnect = ttk.Button(self, text='Disconnect', command=self.disconnect).grid(row=0, column=2, sticky=W)
-		self.btnrqdata = ttk.Button(self, text='Tick', command=self.onetick).grid(row=0, column=1, sticky=W)
+		self.btndisconnect = ttk.Button(self, text='Disconnect', command=self.disconnect).grid(row=1, column=0)
+		
+		self.label_led = Label(self, font=myfont, text="Connection").grid(row=0, column=1, padx=20)
+		#Canvas for drawing circles
+		self.circleCanvas = Canvas(self, width=50, height=50)
+		self.circleCanvas.grid(row=1, column=1)
+		self.circleCanvas.create_oval(10, 10, 40, 40, width=0, fill='red')
 
 		#Tabs
 		n= ttk.Notebook(root, width=550, height=350)
@@ -61,64 +65,45 @@ class Application(Frame):
 		self.tbBid = Entry(f1,font=myfont,width=8,textvariable=varBid).grid(row=3, column=1, sticky=W)
 		self.tbAsk = Entry(f1,font=myfont,width=8,textvariable=varAsk).grid(row=3, column=2, sticky=W)
 
-
 	def cbSymbol_onEnter(self, event):
 		#enables symbol change via dropdown
 		#called by create_widgets
 		varSymbol.set(varSymbol.get().upper())
+	
+	def led_on(self):
+		self.circleCanvas.create_oval(10, 10, 40, 40, width=0, fill='green')
+
+	def led_off(self):
+		self.circleCanvas.create_oval(10, 10, 40, 40, width=0, fill='red')
 
 	def nextValidId_handler(self, msg):
-		#called by tick_csv & onetick
-		global id;
+		#called by tick_csv
 		id = msg.orderId
 		self.makeRequest()
 
 	def error_handler(self, msg):
-		#only print interesting errors
 		#called by tick_csv & onetick
 		if msg.id != None:
 			if msg.id > 0:
 				print(msg)
 
-
-	def my_BidAsk_oneTick(self, msg):
-		#show one tick Bid and Ask quotes
-		#called by onetick
-		global bid,ask
-		if msg.field == 1:
-			bid=msg.price
-			varBid.set(bid)
-		elif msg.field == 2:
-			ask=msg.price
-			varAsk.set(ask)
-
-		if bid != None and ask != None:
-			self.disconnect()
-			bid=None
-			ask=None
-
-	def my_BidAsk(self, msg):
+	def BidAsk(self, msg):
 		#create bid ask csv file
 		#called by tick_csv
-
-		global bid, ask, bidsz, asksz, vol
 		if msg.field == 1:
-			bid=msg.price
+			self.bid=msg.price
 			varBid.set(msg.price)
 		elif msg.field==0:
-			bidsz=msg.size
+			self.bidsz=msg.size
 		elif msg.field == 2:
-			ask=msg.price
+			self.ask=msg.price
 			varAsk.set(msg.price)
 		elif msg.field==3:
-			asksz=msg.size
-		elif msg.field==8:
-			print ('vol: %s' % (msg.size))
-			vol=msg.price
+			self.asksz=msg.size
 
-		#writerow only when data is available
-		if bid != None and ask != None and asksz != None and bidsz != None:
-			writer.writerow([time.strftime("%H:%M:%S"), bid, bidsz, ask, asksz])
+		#TODO: Only write when not empty
+		if self.bid != None and self.bidsz != None and self.ask != None and self.asksz:
+			self.writer.writerow([time.strftime("%H:%M:%S"), self.bid, self.bidsz, self.ask, self.asksz])
 
 	def makeStkContract(self, contractTuple):
 		#called by makeRequest
@@ -135,85 +120,68 @@ class Application(Frame):
 
 	def makeRequest(self):
 		#called by nextValidId_handler
-
-		global tickId
-		tickId = 1
+		self.tickId = 1
 		#contractTuple = ('symbol', 'securityType', 'exchange','primaryExchange', 'currency')
 		contractTuple = ('EUR', 'CASH', 'IDEALPRO','IDEALPRO', varSymbol.get())
 		stkContract = self.makeStkContract(contractTuple)
 		print ('* * * * REQUESTING MARKET DATA * * * *')
 
 		#data request
-		self.con.reqMktData(tickId, stkContract, '', False)
+		self.con.reqMktData(self.tickId, stkContract, '', False)
 
 	def disconnect(self):
 		#linked to button "disconnect"
-		#called by tick_csv & onetick
+		#called by tick_csv
+		connected = varCon.get()
 
-		global status
-		if status == 0: #status = connection status
-			print('* * * * NOT CONNECTED * * * *')
+		if not connected:
+			self.led_off()
 		else:
-			print('* * * * DISCONNECTING * * * *')
-			self.con.cancelMktData(tickId)
+			self.con.cancelMktData(self.tickId)
 			self.con.disconnect()
+			self.led_off()
 			bid = None
 			ask = None
-			status = 0
-
-
-	def onetick(self):
-		#linked to button "tick"
-		global con, status
-
-		#check if connected
-		if status == 1: #status = connection status
-			self.disconnect()
-		status = 1
-
-		#establish connection
-		#port & clientId may need to be altered
-		self.con = ibConnection(port=7497, clientId=325)
-		self.con.register(self.error_handler, message.Error)
-		self.con.register(self.nextValidId_handler, message.nextValidId)
-		self.con.register(self.my_BidAsk_oneTick, message.tickPrice)
-		self.con.connect()
+			connected = FALSE
 
 	def tick_csv(self):
 		#linked to button "csv"
-		global con, status, writer
+		connected = varCon.get()
+
+		#init variables
+		self.bid = None
+		self.bidsz = None
+		self.ask = None
+		self.asksz = None
 
 		#create csv
-		zeit = time.strftime("%d-%m-%Y_%H:%M:%S")
+		init_time = time.strftime("%d-%m-%Y_%H'%M'%S")
 		security = varSymbol.get()
-		myFile = "C:/Users/Tobias/Documents/Coding/TWS_Tick/data/EUR." + str(security) + "_" + str(zeit) +  ".txt"
+		myFile =  "data/EUR." + str(security) + "_" + str(init_time) +  ".txt"
 
 		#create csv header
 		csvheader= ["Time","Bid","Bidsize","Ask","Asksize"]
-		f= open(myFile,"w+")
-		writer = csv.writer(f, delimiter=' ')
-		writer.writerow(csvheader)
+		f= open(str(myFile),"w+")
+		self.writer = csv.writer(f, delimiter=' ')
+		self.writer.writerow(csvheader)
 
 		#check if connected
-		if status == 1:
+		if connected:
 			self.disconnect()
-		status = 1
+			self.led_off()
+		connected = TRUE
+		self.led_on()
 
 		#establish connection
-		self.con = ibConnection(port=7497, clientId=325)
+		self.con = ibConnection(port=myPort, clientId=myclientId)
 		self.con.register(self.error_handler, message.Error)
 		self.con.register(self.nextValidId_handler, message.nextValidId)
-		self.con.register(self.my_BidAsk, message.tickPrice, message.tickSize)
+		self.con.register(self.BidAsk, message.tickPrice, message.tickSize)
 		self.con.connect()
 
-#define variables
-global status
-status = 0 #status = conncection status (1=connected; 0=not connected)
-bid = None
-ask = None
-bidsz = None
-asksz = None
-tickId = None
+#user variables
+myPort = 7497
+myclientId = 325
 
 #finalize GUI
 root = Tk()
@@ -222,6 +190,8 @@ root.geometry('600x480')
 root.attributes('-topmost', True)
 
 #GUI variables
+varCon = BooleanVar(root, value=FALSE)
+CheckVar = BooleanVar(root, value=FALSE)
 varBid= StringVar()
 varAsk= StringVar()
 varSymbol = StringVar(root, value='USD')
